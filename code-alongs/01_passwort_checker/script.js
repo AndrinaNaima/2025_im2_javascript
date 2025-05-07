f345a61822f0aec49a945515a2606c20d3d7efa6
Andrinaleimgruber <andrinaleimgruber@gmail.com>
2025-05-07 11:40:05 +0200

    kanye
    lol

--- code-alongs/01_passwort_checker/script.js
+++ code-alongs/01_passwort_checker/script.js
@@ -1,1 +1,24 @@
 console.log('hi script.js')
+
+let password = prompt("Passwort eingeben"); 
+let username = prompt("Username eingeben"); 
+
+checkStringlength(password, 12);
+checkStringlength(username, 7);
+
+function checkPassword (pw){
+
+if (pw.length>=12)  {
+console.log("Das Passwort ist lang genug.");
+} else {
+    console.log("Bitte verwende ein längeres Passwort.");
+}
+}
+
+function checkStringlength(testString, testLength) {
+    if (testString.length >= testLength)  {
+    console.log("Der Text ist lang genug.");
+    } else {
+        console.log("Bitte verwende ein längeres Passwort.");
+    }
+    }
